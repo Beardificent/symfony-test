9dec7c17ce0eb10810ae3959b3e8e0e97a6e5023
Beardificent <cis.magito@gmail.com>
2020-11-06 11:13:11 +0100

    added function to register the correct name -- with help from Hendrik involving setting variales

--- src/Controller/LearnController.php
+++ src/Controller/LearnController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class LearnController extends AbstractController
 {
 
+    private string $name;
 
     /**
      * @Route("/about-me")
@@ -26,8 +27,14 @@ class LearnController extends AbstractController
      */
     public function showMyName(): Response
     {
+        if(isset($_POST['name'])){
+            $this->name = $_POST['name'];
+        }else{
+            $this->name = 'Unknown';
+        }
+
         return $this->render('learn/showMyName.html.twig', [
-            'name' => 'Unknown',
+            'name' => $this->name,
 
         ]);
 
